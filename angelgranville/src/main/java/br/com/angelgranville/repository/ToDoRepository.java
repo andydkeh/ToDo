81fd5ce91cff26df051d940eb22736aac3022b90
angel-granville <angel.ledebrum@gmail.com>
2026-01-20 14:33:34 -0300

    fix: organização de pastas ajustada

--- angelgranville/src/main/java/br/com/angelgranville/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.angelgranville.repository;
-
-import br.com.angelgranville.entity.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-}
